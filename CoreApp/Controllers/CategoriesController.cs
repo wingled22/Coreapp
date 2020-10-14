c1f9c607f1ad002d4361204b84bacb3efc81f68d
wingled22 <pelayowindel@gmail.com>
2020-10-15 06:57:22 +0800

    ...

--- CoreApp/Controllers/CategoriesController.cs
+++ CoreApp/Controllers/CategoriesController.cs
@@ -49,8 +49,6 @@ namespace CoreApp.Controllers
         }
 
         // POST: Categories/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Name,Description")] Category category)
@@ -81,8 +79,6 @@ namespace CoreApp.Controllers
         }
 
         // POST: Categories/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description")] Category category)
